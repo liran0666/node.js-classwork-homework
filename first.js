f5ec20aebd86efba03dfa213de643155bcc45308
liran0666 <ladelshten@gmail.com>
2025-10-27 16:24:08 +0200

    first lesson first exercise

---
+++ first.js
@@ -0,0 +1,5 @@
+//Liran edelshten& meital israel
+
+//prints in how many numbers (out of 3) the number divides without modulo
+const num = 7;
+console.log(Number(num % 5 == 0) + Number(num % 3 == 0) + Number(num % 2 == 0));
